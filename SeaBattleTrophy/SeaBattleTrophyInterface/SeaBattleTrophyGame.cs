2e24771379ae6570a3ffb40218c1b8f6caaba0a3
MathiasBrannstrom <mathias.brannstrom@gmail.com>
2016-10-30 21:44:19 +0100

    Change shape of landmass

--- SeaBattleTrophy/SeaBattleTrophyInterface/SeaBattleTrophyGame.cs
+++ SeaBattleTrophy/SeaBattleTrophyInterface/SeaBattleTrophyGame.cs
@@ -39,7 +39,7 @@ namespace SeaBattleTrophyGame
         {
             var landMasses = new List<LandMass>();
 
-            landMasses.Add(new LandMass(new[] { new Point2D(100, 100), new Point2D(150, 200), new Point2D(200, 100) }));
+            landMasses.Add(new LandMass(new[] { new Point2D(100, 0), new Point2D(110, 180), new Point2D(150, 200), new Point2D(190,180), new Point2D(200, 0) }));
             landMasses.Add(new LandMass(new[] { new Point2D(50, 200), new Point2D(60, 210), new Point2D(60, 260), new Point2D(24, 240), new Point2D(20,210)}));
             SeaMap = new SeaMap { SizeInMeters = 300, LandMasses = landMasses };
         }
