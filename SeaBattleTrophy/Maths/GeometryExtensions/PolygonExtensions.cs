676f5cdcbf3d7701cee4e3678c9da7cd5ec4bada
MathiasBrannstrom <mathias.brannstrom@gmail.com>
2016-10-31 19:41:12 +0100

    Add GetLineSegments method with yield to Polygon

--- SeaBattleTrophy/Maths/GeometryExtensions/PolygonExtensions.cs
+++ SeaBattleTrophy/Maths/GeometryExtensions/PolygonExtensions.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Maths.Geometry
 {
     public static class PolygonExtensions
     {
+        public static IEnumerable<LineSegment> GetLineSegments(this Polygon polygon)
+        {
+            for (int i = 0; i < polygon.Count; i++)
+            {
+                var p0 = polygon[i];
+                var p1 = polygon[(i + 1) % polygon.Count];
+
+                yield return new LineSegment(p0, p1);
+            }
+        }
+
         public static double ShortestDistanceToPoint(this Polygon polygon, Point2D point)
         {
             Point2D pointOnPolygon;
@@ -25,13 +37,8 @@ namespace Maths.Geometry
         {
             var minSquaredDistance = double.MaxValue;
             pointOnPolygon = new Point2D();
-            for (int i = 0; i < polygon.Count; i++)
+            foreach (var lineSegment in polygon.GetLineSegments())
             {
-                var p0 = polygon[i];
-                var p1 = polygon[(i + 1) % polygon.Count];
-
-                var lineSegment = new LineSegment(p0, p1);
-
                 Point2D candidateForPointOnPolygon;
                 var squaredDis = lineSegment.ShortestSquaredDistanceToPoint(point, out candidateForPointOnPolygon);
 
